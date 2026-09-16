b16e01b0b585cac3cd7cb9e678d0ac7c6cec73cb
Itaham05 <mahati.0902@gmail.com>
2026-09-16 12:05:45 +0530

    edited url

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://atomism-frontend.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://atomism-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
